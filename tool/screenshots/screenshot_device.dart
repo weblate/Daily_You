3c56f29b71c653b0921e9b08df45e73ca7c33819
Demizo <demizodemazo@gmail.com>
2026-09-25 22:22:58 -0500

    feat: increase scaling in screenshot tool

--- tool/screenshots/screenshot_device.dart
+++ tool/screenshots/screenshot_device.dart
@@ -3,7 +3,8 @@ import 'package:golden_screenshot/golden_screenshot.dart';
 final fastlanePhoneDevice = ScreenshotDevice(
   platform: GoldenScreenshotDevices.androidPhone.device.platform,
   resolution: GoldenScreenshotDevices.androidPhone.device.resolution,
-  pixelRatio: GoldenScreenshotDevices.androidPhone.device.pixelRatio,
+  pixelRatio: GoldenScreenshotDevices.androidPhone.device.resolution.width /
+      (1080 / 2.625),
   goldenSubFolder: GoldenScreenshotDevices.androidPhone.device.goldenSubFolder,
   frameBuilder: ScreenshotFrame.noFrame,
 );
